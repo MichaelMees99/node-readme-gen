ffa9a2f37d9573c2aaf2266f33b798f13a9c14db
MichaelMees99 <120432979+MichaelMees99@users.noreply.github.com>
2023-05-29 20:31:19 -0400

    Functionally complete

---
+++ index.js
@@ -0,0 +1,66 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Write a short description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions for this project?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Describe how you use this project?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Please choose a license:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Who are the contributors of this project?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What are the tests for this project?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email:',
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README.md!')
+  );
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    writeToFile('README.md', generateMarkdown(answers));
+  });
+}
+
+init();
